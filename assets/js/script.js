97d8497d9bb1a071f3fb4159540b7945574c726c
Seraphina <etanschool@gmail.com>
2022-04-06 11:27:12 -0400

    aside functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,20 @@
-var pf = new petfinder.Client({apiKey: "RA4n7SbRFoVOEcgaMyrMS4sGV3i12K8GAYWg5x1pJ99IP3RvCh", secret: "udoTfKJrQ727p6MPRYUtPAmeTZSxrHY796AljjgM"});
+//var pf = new petfinder.Client({apiKey: "RA4n7SbRFoVOEcgaMyrMS4sGV3i12K8GAYWg5x1pJ99IP3RvCh", secret: "udoTfKJrQ727p6MPRYUtPAmeTZSxrHY796AljjgM"});
+var locationData = document.querySelector('#location-data')
 
 
 
  
-pf.animal.search()
-.then(function (response) {
-var animalArr = response.data.animals
-for(var i = 0; i < animalArr.length; i++){
-    
-}
-});
+// pf.animal.search()
+// .then(function (response) {
+// var animalArr = response.data.animals
+// for(var i = 0; i < animalArr.length; i++){
+//     console.log(animalArr[i])  
+// }
+// });
 
+
+$('#location-submit').submit(function(event) {
+    event.preventDefault();
+    console.log('?');
+    console.log(locationData.value)
+})
