298affe651e10b78d2320e2ae10bc4475f607c63
noah gaston <noahsfuture18@gmail.com>
2022-04-06 11:16:46 -0400

    shows selection of dogs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,103 @@
+// how we are able to get the data within the api
+
 var pf = new petfinder.Client({apiKey: "RA4n7SbRFoVOEcgaMyrMS4sGV3i12K8GAYWg5x1pJ99IP3RvCh", secret: "udoTfKJrQ727p6MPRYUtPAmeTZSxrHY796AljjgM"});
 
-
-
- 
+// searches through the api for animals
 pf.animal.search()
-.then(function (response) {
-var animalArr = response.data.animals
-for(var i = 0; i < animalArr.length; i++){
+    .then(function (response) {
+        // takes in all the animal data objects
+        var animals = response.data.animals
+        // an empty array that will take in only the dogs up for adoption
+        var dogArr = [];
+        // loops through all the animlas data.
+        for(let i = 0; i < animals.length; i++) {
+            // singles out dogs and pushes them into the dog array
+            if(animals[i].type == "Dog"){
+                dogArr.push(animals[i])
+        }  
+    }
+        // loops through the dog arry
+        for(let i = 0; i < dogArr.length; i++){
+
+            if(i == i){
+                // looks to see if the data includes a photo of the dog
+                var maybePhoto = dogArr[i].primary_photo_cropped
+                // 
+                if(maybePhoto == null){
+                } else {
+                    maybePhoto = dogArr[i].primary_photo_cropped.small
+                }
+                
+                // gets the dog name for current object
+                var dogName = dogArr[i].name
+                // gets the dog breed for current object
+                var dogBreed = dogArr[i].breeds.primary
+                // gets the dog gender for current object
+                var dogGender = dogArr[i].gender
+                // gets the link to the dog
+                var url = dogArr[i].url
+
+
+                // creates the card dive
+               var cardDiv = $('<div>')
+                // adds the class of card to the newly made div  
+               cardDiv.addClass('card')
+
+                //creates the img div
+               var imgDiv = $('<div>')
+                // creates the class of card-image to the newly made div
+               imgDiv.addClass('card-image')
+                // creates an image element
+               var img = $('<img>')
+                // adds the path to the photo within the data to an src attribute
+               img.attr('src', maybePhoto)
+                // appends the image to the image div
+               imgDiv.append(img)
+                
+                // creates the content div
+               var contentDiv = $('<div>')
+                // adds the class of card-content to the newly made div
+               contentDiv.addClass('card-content')
+                // creates an h3 element
+               var h3El = $('<h3>')
+                // adds the dogs name to the h3 element
+               h3El.text(dogName)
+                // appends the h3 element to the content div
+               contentDiv.append(h3El)
+
+                // creates a ul element
+               var ulEl = $('<ul>')
+                // creates a li element
+               var liEl = $('<li>')
+                // appesnds the li to the ul
+               ulEl.append(liEl)
+                // sets the li text to the dogs breed
+               liEl.text(dogBreed)
+                // appends the ul element to the content div
+               contentDiv.append(ulEl)
+
+               var linkDiv = $('<div>')
+               linkDiv.addClass('card-action')
+               var aEl = $('<a>')
+               aEl.attr('href', url)
+               aEl.text('Find more here')
+               linkDiv.append(aEl)
+
+               
+ 
+
+
+
+
+                cardDiv.append(imgDiv, contentDiv, linkDiv )
+                var cards = $('#cards')
+
+                cards.append(cardDiv)  
+            }
+        }
+   
+
     
-}
+    
 });
 
